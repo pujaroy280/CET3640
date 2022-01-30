bf786abf8dc0992dd7c4dc56045e2e823784c4e2
Puja Roy <62675121+pujaroy280@users.noreply.github.com>
2022-01-30 00:13:32 -0500

    Add files via upload

---
+++ HelloPuja.java
@@ -0,0 +1,31 @@
+public class HelloPuja {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        System.out.print("Hello everybody ");
+        // ln means new line 
+        System.out.println("My first program");
+        System.out.println("My name is Puja Roy");
+        System.out.println("I live in New York");
+        System.out.println("I am taking CET 3640 for the Spring 2022 semester");
+        System.out.println("I want to learn how to program in Java");
+        System.out.println("During my free time, I enjoy learning how to code");
+        System.out.println("My favorite programming language is Python");
+        System.out.println("I hope to learn about Java and JavaScript");
+        System.out.println("I want to be a programmer");
+        System.out.println("My favorite food is chicken with rice");
+        System.out.println("I like watching documentaries");
+        System.out.println("I like reading articles");
+        System.out.println("Engineering is fun!");
+        System.out.println("I like to cook a variety of foods");
+        System.out.println("I love animals");
+        System.out.println("Coding in Java is fun");
+        System.out.println("I am currently 20 years old");
+        System.out.println("My favorite coffee shop is Starbucks");
+        System.out.println("I like drinking Iced Matcha Latte or Caramel Frappe");
+        System.out.println("I like feeding pigeons");
+        System.out.println("I am interested in AI/Machine Learning and Data Science");
+        System.out.println("I am learning how to drive a car");
+	}
+}
