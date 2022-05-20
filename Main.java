d02f9e8e615a88915d1d02b767332f5bfeeaf997
Puja Roy <62675121+pujaroy280@users.noreply.github.com>
2022-05-19 21:34:14 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,45 +1,33 @@
-import java.util.Scanner;
-
 //NAME: Puja Roy
-//DATE: 3/12/22
-// This program calculates hours worked, hours paid, the gross salary of the job. It also provides information of how much federal tax, state tax and net pay the employee earns. 
-public class Main {
+//DATE: 5/19/22
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.util.*;
 
-		Scanner sc = new Scanner(System.in);
-        String firstName;
-        System.out.println("Enter your first name: ");   // Input First Name
-        firstName = sc.nextLine();
-        String lastName;
-        System.out.println("Enter your last name: ");    // Input Last Name 
-        lastName = sc.nextLine();
-        int hoursWorked;
-        System.out.println("Enter the number of hours worked: ");  // Input # of hours worked
-        hoursWorked = sc.nextInt();
-        double hourlyRate;
-        System.out.println("Enter the hourly rate: ");   // Input hourly rate of the job
-        hourlyRate = sc.nextDouble();
-
-        double grossSalary;    
-        if (hoursWorked <= 40) {                         // Calculates gross salary by multiplying hourly rate and hours worked
-            grossSalary = hourlyRate * hoursWorked;
-        } else {
-            grossSalary = 40 * hourlyRate + (hoursWorked - 40) * 1.5 * hourlyRate;
-        }
-        double federalTax = 0.2 * grossSalary;
-        double stateTax = 0.05 * grossSalary;
-        double netPay = grossSalary - federalTax - stateTax;
-
-        System.out.println("Name: " + firstName + " " + lastName);   // Prints the output of the code 
-        System.out.println("Hours worked: " + hoursWorked);
-        System.out.println("Hourly Paid: " + hourlyRate);
-        System.out.println("The gross salary is: " + grossSalary);
-        System.out.println("Federal tax: " + federalTax);
-        System.out.println("State tax: " + stateTax);
-        System.out.println("Net Pay: " + netPay);	
-		
-	}
-
+public class Main
+{
+static void longest(Stack<String> st)
+{
+String longest="";//longest string is set to null
+while(!st.isEmpty())//loop until stack of strings becomes empty
+{
+String s=st.pop();//remove each element
+if(s.length()>longest.length())// Verifies which string in each stack is larger
+{
+longest=s;//if it is larger then executes the word (string)
 }
+}
+System.out.println(longest);//Prints the largest or longest string of the stack 
+}
+   public static void main(String[] args) {
+       Stack<String> st= new Stack<>();
+       String []str={"table","chair","computer","hi","hello","Student","Joseph","King","Queen","Rosie"};
+       for(int i=0;i<str.length;i++)//you can push whole array if the size is large also
+       {
+       st.push(str[i]);//push 10 elements to stack
+       }
+       System.out.println(st);//stack values
+       longest(st);//call function
+   }
+}
+
+
